bc11d4830175eb3905ff0e535e8c73ab057aaff3
Patrick Walsh <pw4.2020@gmail.com>
2023-10-24 08:47:32 -0400

    configure test_mutationAdequate.py to be 94% code coverage

--- test_suit/test_mutationAdequate.py
+++ test_suit/test_mutationAdequate.py
@@ -14,23 +14,22 @@ class TestMutants(unittest.TestCase):
     def test_scalene(self):
         """Test that the mutant is killed upon
         a^2+b^2=c^2."""
-        # try:
-        #     self.assertEqual(Triangle.classify(5, 10, 15), Triangle.Type.SCALENE)
-        # except AssertionError as exc:
-        #     return
-        self.assertEqual(Triangle.classify(24, 10, 15), Triangle.Type.SCALENE)
+        self.assertEqual(Triangle.classify(26, 14, 15), Triangle.Type.SCALENE)
 
-    # def test_equilateral(self):
-    #     """Test that the mutant is killed upon
-    #     a==b==c."""
-    #     self.assertEqual(Triangle.classify(1, 1, 1), Triangle.Type.EQUILATERAL)
+    def test_invalid_lengths(self):
+        self.assertEqual(Triangle.classify(1, 3, 5), Triangle.Type.INVALID)
+        self.assertEqual(Triangle.classify(1, 5, 3), Triangle.Type.INVALID)
+        self.assertEqual(Triangle.classify(3, 1, 5), Triangle.Type.INVALID)
 
-    # def test_isoceles(self):
-    #     """Test that the mutant is killed upon
-    #     a==b."""
-    #     self.assertEqual(Triangle.classify(2, 2, 3), Triangle.Type.ISOSCELES)
-    #     self.assertEqual(Triangle.classify(3, 2, 2), Triangle.Type.ISOSCELES)
-    #     self.assertEqual(Triangle.classify(3, 2, 3), Triangle.Type.ISOSCELES)
+    def test_equilateral(self):
+        self.assertEqual(Triangle.classify(3, 3, 3), Triangle.Type.EQUILATERAL)
+
+    def test_isoceles(self):
+        """Test that the mutant is killed upon
+        a==b."""
+        self.assertEqual(Triangle.classify(2, 2, 3), Triangle.Type.ISOSCELES)
+        self.assertEqual(Triangle.classify(3, 2, 2), Triangle.Type.ISOSCELES)
+        self.assertEqual(Triangle.classify(3, 2, 3), Triangle.Type.ISOSCELES)
 
 
 if __name__ == "__main__":
